cb8e89b2a3e75329ddc1483b7ca906f370f88849
root <samb8104@gmail.com>
2020-06-01 14:06:04 +0100

    Log directory verification

--- src/utils/file.js
+++ src/utils/file.js
@@ -7,6 +7,28 @@ const fs = require("fs");
 
 class File {
 
+    //Check log directories exist and whether to create them if not (create:bool)
+    verifyLogs(create) {
+        return new Promise((resolve, reject) => {
+            this.paths = ["./logs/backup", "./logs/system"];
+            this.count = 0;
+            this.paths.forEach(path => {
+                if(fs.existsSync(path)) { this.count++ } 
+                else { 
+                    if (create) {
+                        try { 
+                            fs.mkdirSync(path, {recursive: true});
+                            fs.writeFileSync(`${path}/.gitignore`, "*\n!.gitignore");
+                            this.count++
+                        } catch (error) {}
+                    }
+                }
+            })
+            if (this.count == this.paths.length) { resolve() }
+            else { reject() }
+        })
+    }
+
 }
 
-module.exports = new File();
+module.exports = File;
