dfc215ad39899d3acb4c0c07ea1fe6127d791130
root <samb8104@gmail.com>
2020-06-02 14:03:33 +0100

    Base64 id generator

---
+++ src/utils/maths.js
@@ -0,0 +1,29 @@
+/*
+
+Math related utils
+
+*/
+
+class Maths {
+
+    //Generate a base64 string of specified length
+    generateID(length) {
+        return new Promise((resolve, reject) => { 
+            try {
+
+                this.chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-_";
+                this.id = "";
+                this.rNums = [];
+                
+                for (let i=0; i <= length; i++) { this.rNums.push(Math.round(Math.random() * 64)) }
+                this.rNums.forEach((num) => { this.id = this.id + this.chars[num] })
+
+                resolve(this.id)
+
+            } catch (error) { reject(error.message) }
+        })
+    }
+
+}
+
+module.exports = Maths;
