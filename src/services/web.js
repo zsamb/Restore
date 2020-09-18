6c80a118ba425c77d202ea67418e87bf62a3d220
Samb8104 <samb8104@gmail.com>
2020-09-18 13:52:07 +0000

    Port in use retries and controlled shutdowns

--- src/services/web.js
+++ src/services/web.js
@@ -5,6 +5,8 @@ const express = require("express");
 const app = express();
 const Mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+const http = require("http");
+const path = require("path");
 
 const Log = require("../utils/log");
 
@@ -12,8 +14,6 @@ const UserRouter = require("../routers/user");
 const GroupRouter = require("../routers/group");
 const BackupRouter = require("../routers/backup");
 const FrontendRouter = require("../routers/frontend");
-const Auth = require("../middleware/auth");
-const path = require("path");
 
 let connectionURI = process.argv[2];
 let config = JSON.parse(process.argv[3]);
@@ -29,9 +29,8 @@ try {
     }).then(() => {
         Log.send("system", "Connected to MongoDB");
 
-        //Configure express
+        //Configure express to use JSON, handle parsing errors
         app.use(express.json())
-        //Catch parsing errors from above
         app.use((err, req, res, next) => {
             if (err instanceof SyntaxError && err.status >= 400 && err.status < 500 && err.message.indexOf('JSON') !== -1) {
                 res.status(400).send({error: true, data: "Invalid JSON input"});
@@ -55,11 +54,46 @@ try {
         app.use(FrontendRouter);
 
         //Start
-        app.listen(config.web.port, () => Log.send("system", "Webserver started", { colour: "FgGreen" }));
+        const httpServer = http.createServer(app);
 
+        let errored = false;
+        let retries = 1;
+
+        httpServer.on("error", (error) => {
+            if (errored) {
+                Log.send("system", `Failed to start webserver: ${error.message}`, { error: true, colour: "FgRed"});
+                process.exit();
+            } else if (error.code == "EADDRINUSE") {
+                if (retries == 3) { errored = true } else { retries++ }
+                Log.send("system", `Port ${config.web.port} is already in use, retrying in 5s..`, { error: true, colour: "FgRed"});
+                setTimeout(() => {
+                    httpServer.listen(config.web.port, () => Log.send("system", "Webserver started", { colour: "FgGreen" }) )
+                }, 5000);
+            } else { 
+                Log.send("system", `Failed to start webserver: ${error.message}`, { error: true, colour: "FgRed"});
+                process.exit();
+            }
+        })
+
+        httpServer.listen(config.web.port, () => Log.send("system", "Webserver started", { colour: "FgGreen" }) );
+
+        //Close the webserver before exit and managed all possible exits
+        const exitCode = () => {
+            if (httpServer) { 
+                Log.send("system", "Closing webserver...");
+                httpServer.close(() => { process.exit() });
+                setTimeout(() => { process.exit()}, 2000).unref();      //Force close after 2s
+            }
+        }
+
+        process.on("SIGINT", () => { exitCode() });
+        process.on("SIGTERM",() => { exitCode() });
+        process.on("uncaughtException", (error) => { 
+            Log.send("system", `Web server closed due to an error: ${error.message}`, {error: true, colour: "FgRed"});
+            exitCode();  
+        });
 
     }).catch(error => { throw new Error(`Failed to connect to MongoDB: ${error.message}`)});
-
 } catch (error) {
     Log.send("system", `Webserver encountered an error: ${error.message}`, { error: true, colour: "FgRed"});
 }
